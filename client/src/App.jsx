18231c24cd6f6e834e7936a946c5ec8b323ceeca
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-02-03 12:04:06 -0500

    making routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,37 @@
-
+import { useEffect } from "react"
+import { BrowserRouter, Route , Routes, useLocation} from "react-router-dom"
+import { CheckOut, Confirmation, Home } from "./scenes"
+import ItemDetails from "./scenes/itemDetails/ItemDetails"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const ScrollToTop = () => {
+
+    
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+      window.scrollTo(0,0)
+    }, [pathname])
+    
+    return null
+  }
+
+  
+  
 
   return (
     <div className="app">
-
+      < BrowserRouter >
+        < ScrollToTop />
+        < Routes>
+          < Route path="/" element={< Home />} /> 
+          < Route path="/item/:itemId" element={< ItemDetails />} />
+          < Route path="/checkout" element={< CheckOut />} /> 
+          < Route path="/checkout/success" element={ < Confirmation />} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ client/src/scenes/index.js
@@ -0,0 +1,15 @@
+import Home from "./home/Home"
+import NavBar from "./global/NavBar.jsx"
+import CheckOut from "./checkout/CheckOut"
+import ItemsDetails from "./itemDetails/ItemDetails"
+import Confirmation from "./checkout/Confirmation"
+
+
+
+export {
+  Home,
+  NavBar,
+  CheckOut,
+  Confirmation,
+  ItemsDetails
+}
